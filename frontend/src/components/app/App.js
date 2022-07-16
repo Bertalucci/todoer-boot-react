54c878a46ac12a73f899dc3bf01c198d44e1a0fc
Bertalucci <bertamalfitano@gmail.com>
2022-07-16 16:19:48 +0300

    Recreate frontend layer

---
+++ frontend/src/components/app/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component {
+  state = {
+    clients: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/clients');
+    const body = await response.json();
+    this.setState({clients: body});
+  }
+
+  render() {
+    const {clients} = this.state;
+    return (
+        <div className="App">
+              <h2>Clients</h2>
+              {clients.map(client =>
+                  <div key={client.id}>
+                    {client.name} ({client.email})
+                  </div>
+              )}
+        </div>
+    );
+  }
+}
+export default App;
